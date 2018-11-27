297ef8566c14c9af6d1c7f78b9bc5579af690fde
Александр Горячев <@local.itstep>
2018-11-27 21:26:26 +0600

    Добавьте файлы проекта.

---
+++ Project5/Source.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+float main()
+{
+
+	setlocale(LC_ALL, "");
+	
+	/*float Tf, Tc;
+	printf("T c*:  ");
+	scanf_s("%f", &Tf);
+	Tc = (Tf-32)*5 / 9;
+	printf("result: %f\n", Tc);*/
+
+
+
+
+	/*int A, A2, A3, A5, A10, A15;
+	A = 4;
+	A2 = pow(A, 2);
+	A3 = A2 * A;
+	A5 = A2 * A3;
+	A10 = pow(A5, 2);
+	A15 = A10 * A5;
+	printf("result: %d\n", A2);
+	printf("result: %d\n", A3);
+	printf("result: %d\n", A5);
+	printf("result: %d\n", A10);
+	printf("result: %d\n", A15);*/
+
+
+
+	
+	
+	/*int A, A2, A4, A8;
+	A = 4;
+	A2 = A * A;
+	A4 = A2 * A2;
+	A8 = A4 * A4;
+	printf("result: %d\n", A8);
+	system("pause");
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	/*int y, x;
+
+	x = 3;
+
+	y = 4 * (x - 3)*(x - 3)*(x - 3)*(x - 3)*(x - 3)*(x - 3) - 7 * (x - 3)*(x - 3)*(x - 3) + 2;
+
+	printf("result: %d\n", y);
+	system("pause");
+	*/
+	
+	
+	
+	
+	
+	
+	/*int y, x;
+
+	x = 3;
+
+	y = 3 * x*x*x*x*x*x - 6 * x*x - 7;
+
+	printf("result: %d\n", y);*/
+	system("pause");
+
+
+
+
+
+
+
+
+
+
+
+}
